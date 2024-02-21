de1b831e146c1cacaa9fe3a27afc802b1f4cbd77
KMS <rlaaltjd46@naver.com>
2024-02-21 23:43:53 +0900

    feat : 인트로씬에 메뉴 버튼 및 하위버튼 UI생성까지

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -3,9 +3,51 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    const string MAIN_SCENE_NAME = "MainScene";
+    private static UIManager instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindFirstObjectByType<UIManager>();
+
+                if (instance == null)
+                {
+                    GameObject managerObject = new("UIManager");
+                    instance = managerObject.AddComponent<UIManager>();
+                }
+            }
+            return instance;
+        }
+    }
+    [HideInInspector]
+    public bool isTurnOnSound = true;
+
+    private const string MAIN_SCENE_NAME = "MainScene";
+    private bool isOpenMenu = false;
+    private bool isOpenExitAlert = false;
+
+    private GameObject menuGroup;
+
+    void Awake()
+    {
+        menuGroup = GameObject.Find("Canvas").transform.Find("Menu").transform.Find("MenuGroup").gameObject;  
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(MAIN_SCENE_NAME);
     }
+
+    public void ToggleMenu()
+    {
+        isOpenMenu = !isOpenMenu;
+        menuGroup.SetActive(isOpenMenu);
+    }
+
+    public void ToggleSound(bool isTurnOnSound) { }
+    public void OpenGameExitAlert() { }
+    public void GoIntroScene() { }
+
+    public void ToggleInfomation(bool isOpen) { }
 }
